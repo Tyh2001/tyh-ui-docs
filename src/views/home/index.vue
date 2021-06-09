689a78bd65aa0e72acebca4786846e684e9f28a6
unknown <you@example.com>
2021-06-09 10:35:09 +0800

    首页完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -8,15 +8,122 @@
     </div>
 
     <div class="onStart">
+      <!-- 快速开始按钮 -->
       <div class="btnBox">
         <Tyh-button type="success" round @click="$router.push('/component')">
           快速开始
         </Tyh-button>
       </div>
+
+      <!-- 概述 -->
+      <div class="summary">
+        <div class="summary-left">
+          <h2 class="summary-title">开发靠个人</h2>
+          <p class="summary-content">
+            Tyh-ui 是由 tyh2001 独立开发的一款基于 Vue 2.0 的桌面端轻量级 UI
+            组件库。
+            <br /><br />
+            如果您在使用中有什么问题或 BUG 不妨可以来仓库的 Lssuse
+            中反馈一下，我很乐意来进行改进。
+          </p>
+        </div>
+        <div class="summary-right">
+          <h2 class="summary-title">轻量易维护</h2>
+          <p class="summary-content">
+            Tyh-ui
+            是一个极其轻量的组件库，它并没有那么多复杂的目录结构和冗余的代码，清晰明了。
+            <br /><br />
+            但同时 Tyh-ui
+            还有很多地方需要维护改进，远远不够完美，我正在努力改进中。
+          </p>
+        </div>
+      </div>
     </div>
 
     <!-- 页脚 -->
-    <div class="footer">这是页脚内容</div>
+    <div class="footer">
+      <!-- 合作者 -->
+      <div id="Collaborators">
+        <h4>合作者</h4>
+        <Tyh-link target="_blank" url="https://github.com/Tyh2001">
+          <div class="userBox">
+            <img class="myPhoto" src="./images/我的头像.jpg" alt="" />
+            <span class="userName">Tyh2001</span>
+          </div>
+        </Tyh-link>
+      </div>
+      <!-- 链接 -->
+      <div id="footer-link">
+        <!-- 链接 -->
+        <div class="link">
+          <h4 class="lint-title">链接</h4>
+          <ul class="link-ul">
+            <li>
+              <Tyh-link target="_blank" url="https://github.com/Tyh2001"
+                >Github</Tyh-link
+              >
+            </li>
+            <li>
+              <Tyh-link target="_blank" url="https://github.com/Tyh2001/tyh-ui"
+                >源码仓库</Tyh-link
+              >
+            </li>
+            <li>
+              <Tyh-link
+                target="_blank"
+                url="https://github.com/Tyh2001/tyh-ui/issues"
+                >Lssues</Tyh-link
+              >
+            </li>
+            <li>
+              <Tyh-link
+                target="_blank"
+                url="https://github.com/Tyh2001/tyh-ui-web"
+                >官网仓库</Tyh-link
+              >
+            </li>
+            <li>
+              <Tyh-link
+                target="_blank"
+                url="https://www.npmjs.com/package/tyh-ui"
+                >NPM</Tyh-link
+              >
+            </li>
+          </ul>
+        </div>
+
+        <!-- 相关文档 -->
+        <div class="link">
+          <h4 class="lint-title">相关文档</h4>
+          <ul class="link-ul">
+            <li>
+              <Tyh-link target="_blank" url="https://element.eleme.cn/#/zh-CN"
+                >Element</Tyh-link
+              >
+            </li>
+            <li>
+              <Tyh-link target="_blank" url="https://www.iviewui.com"
+                >View UI</Tyh-link
+              >
+            </li>
+            <li>
+              <Tyh-link
+                target="_blank"
+                url="https://at-ui.github.io/at-ui/#/zh/"
+                >at-ui</Tyh-link
+              >
+            </li>
+            <li>
+              <Tyh-link
+                target="_blank"
+                url="https://youzan.github.io/vant/#/zh-CN/quickstart"
+                >Vant</Tyh-link
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -52,7 +159,7 @@ export default {
   }
   // 主横幅
   .Banner {
-    width: 650px;
+    width: 800px;
     margin: auto;
     margin-top: 30px;
     img {
@@ -62,6 +169,7 @@ export default {
   // 快速开始
   .onStart {
     height: 400px;
+    // 开始按钮
     .btnBox {
       width: 200px;
       margin: auto;
@@ -71,11 +179,87 @@ export default {
         height: 50px;
       }
     }
+    // 概述
+    .summary {
+      width: 800px;
+      height: 200px;
+      border-top: 1px solid rgb(219, 219, 219);
+      margin: auto;
+      margin-top: 60px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .summary-left,
+      .summary-right {
+        width: 50%;
+        height: 200px;
+        // border: 1px solid black;
+        padding: 25px;
+        box-sizing: border-box;
+        .summary-title {
+          line-height: 60px;
+          color: #2d3035;
+          font-weight: 500;
+        }
+        .summary-content {
+          color: #515a6e;
+          line-height: 25px;
+        }
+      }
+    }
   }
   // 页脚
   .footer {
-    height: 300px;
     background: #f7fbfd;
+    padding: 60px 200px;
+    box-sizing: border-box;
+    #footer-link {
+      display: flex;
+      padding: 30px 0;
+      border-bottom: 1px solid rgb(219, 219, 219);
+      .link {
+        width: 180px;
+        .link-ul {
+          margin-top: 27px;
+          li {
+            list-style: none;
+            line-height: 30px;
+          }
+        }
+      }
+    }
+    // 合作者
+    #Collaborators {
+      padding: 30px 0;
+      border-bottom: 1px solid rgb(219, 219, 219);
+      .tyh-link {
+        width: 150px;
+        height: 50px;
+        display: inline-block;
+        margin-top: 20px;
+        .userBox {
+          width: 150px;
+          height: 50px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          cursor: pointer;
+          // 头像
+          .myPhoto {
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+          }
+          // 用户昵称
+          .userName {
+            font-size: 20px;
+            font-weight: 600;
+            color: #515a6e;
+            margin-left: 10px;
+          }
+        }
+      }
+    }
   }
 }
 </style>
