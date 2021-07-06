a6f3abfecf6fbc091fe2615bc68eb96abbe187bb
unknown <you@example.com>
2021-07-06 11:37:50 +0800

    更新全新的组件

--- src/components/special/button.vue
+++ src/components/special/button.vue
@@ -1,14 +1,62 @@
 <template>
-  <div>特别的按钮 后续更新。。。</div>
+  <div id="component-item">
+    <h1 class="title">Button 按钮</h1>
+
+    <h3 class="Subtitle">基本使用：</h3>
+    <p class="explain">基础使用</p>
+    <Tyh-button-up type="primary">主要按钮</Tyh-button-up>
+    <Tyh-button-up type="success">成功按钮</Tyh-button-up>
+    <Tyh-button-up type="danger">危险按钮</Tyh-button-up>
+    <Tyh-button-up type="warning">警告按钮</Tyh-button-up>
+    <pre v-highlightjs>
+      <code id="myCode" class="html">
+&lt;Tyh-button-up type="primary"&gt;主要按钮&lt;/Tyh-button-up&gt;
+&lt;Tyh-button-up type="success"&gt;成功按钮&lt;/Tyh-button-up&gt;
+&lt;Tyh-button-up type="danger"&gt;危险按钮&lt;/Tyh-button-up&gt;
+&lt;Tyh-button-up type="warning"&gt;警告按钮&lt;/Tyh-button-up&gt;
+      </code>
+    </pre>
+
+    <!-- 配置项 -->
+    <h3 class="Subtitle">配置项：</h3>
+    <ConfiguresTable :configures="configures" />
+
+    <h3 class="Subtitle">按需引入：</h3>
+    <p class="explain">
+      通过下面按需引入的方式，我们可以只引入需要的组件，以达到减小项目体积的目的
+    </p>
+    <pre v-highlightjs>
+      <code id="myCode" class="js">
+import Vue from 'vue'
+import TyhButtonUp from 'tyh-ui/packages/button-up'
+Vue.use(TyhButtonUp)
+      </code>
+    </pre>
+
+    <TurnPage :TurnPageList="TurnPageList" />
+  </div>
 </template>
 
 <script>
+import ConfiguresTable from '../components/ConfiguresTable'
+import TurnPage from '../components/TurnPage'
 export default {
   name: '',
-  components: {},
+  components: {
+    ConfiguresTable,
+    TurnPage
+  },
   props: {},
   data () {
-    return {}
+    return {
+      configures: [
+        { param: 'type', explain: '按钮的类型', type: 'string', value: 'primary / success / danger / warning', default: '——' }
+      ],
+      TurnPageList: [
+        { text: '组件', url: '/component' },
+        { text: 'Input 输入框', url: '/special/input' }
+      ]
+    }
   },
   computed: {},
   watch: {},
--- src/components/special/input.vue
+++ src/components/special/input.vue
@@ -1,14 +1,52 @@
 <template>
-  <div>特别的输入框 后续更新。。。</div>
+  <div id="component-item">
+    <h1 class="title">Input 输入框</h1>
+
+    <h3 class="Subtitle">基本使用：</h3>
+    <p class="explain">基础使用</p>
+
+    <pre v-highlightjs>
+      <code id="myCode" class="html">
+
+      </code>
+    </pre>
+
+    <!-- 配置项 -->
+    <h3 class="Subtitle">配置项：</h3>
+    <ConfiguresTable :configures="configures" />
+
+    <h3 class="Subtitle">按需引入：</h3>
+    <p class="explain">
+      通过下面按需引入的方式，我们可以只引入需要的组件，以达到减小项目体积的目的
+    </p>
+    <pre v-highlightjs>
+      <code id="myCode" class="js">
+
+      </code>
+    </pre>
+
+    <TurnPage :TurnPageList="TurnPageList" />
+  </div>
 </template>
 
 <script>
+import ConfiguresTable from '../components/ConfiguresTable'
+import TurnPage from '../components/TurnPage'
 export default {
   name: '',
-  components: {},
+  components: {
+    ConfiguresTable,
+    TurnPage
+  },
   props: {},
   data () {
-    return {}
+    return {
+      configures: [],
+      TurnPageList: [
+        { text: 'Button 按钮', url: '/special/button' },
+        { text: '', url: '/special/' }
+      ]
+    }
   },
   computed: {},
   watch: {},
